5b59a439d5f900ab3423ab5ad85caa52f06d50a0
dawnzs01 <18013865675@163.com>
2023-10-29 23:06:56 +0800

    curator-fix

--- providers/mongo/shedlock-provider-mongo/src/main/java/net/javacrumbs/shedlock/provider/mongo/package-info.java
+++
@@ -1,6 +0,0 @@
-@NonNullApi
-@NonNullFields
-package net.javacrumbs.shedlock.provider.mongo;
-
-import net.javacrumbs.shedlock.support.annotation.NonNullApi;
-import net.javacrumbs.shedlock.support.annotation.NonNullFields;
--- spring/shedlock-spring/src/main/java/net/javacrumbs/shedlock/spring/package-info.java
+++
@@ -1,6 +0,0 @@
-@NonNullApi
-@NonNullFields
-package net.javacrumbs.shedlock.spring;
-
-import net.javacrumbs.shedlock.support.annotation.NonNullApi;
-import net.javacrumbs.shedlock.support.annotation.NonNullFields;
